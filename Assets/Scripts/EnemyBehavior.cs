d9d034253c7e449f4edcc7e3e925224f01c16878
DOMAN9 <doman9.art@gmail.com>
2025-05-29 14:39:04 +0800

    Assignment2
    finished assignment 2, with gameover screen after enemy collides with player

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -6,9 +6,15 @@ public class EnemyBehavior : MonoBehaviour
     public Transform player;
     public NavMeshAgent agent;
 
+    public GameObject gameoverpanel;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (gameoverpanel != null)
+        {
+            gameoverpanel.SetActive(false);
+        }
         
     }
 
@@ -16,5 +22,17 @@ public class EnemyBehavior : MonoBehaviour
     void Update()
     {
         agent.SetDestination(player.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (gameoverpanel != null)
+            {
+                gameoverpanel.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
     }
 }
